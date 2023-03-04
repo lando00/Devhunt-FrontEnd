e65682d2fee1d38c4e6cd6488d03371a13e7fa98
N-mika <nadrasanamichael.com>
2023-03-04 17:45:24 +0300

    stack

--- src/components/EspaceEtudiant/Header/Header.jsx
+++ src/components/EspaceEtudiant/Header/Header.jsx
@@ -6,16 +6,20 @@ import avatar from '../../../assets/avatars/face-1.jpg'
 export default function Header() {
   return (
     <div className='header'>
+      <div className="name-platforme">ENIfanampy</div>
       <img className='logo-aeeni' src={logoAeeni} alt="logo aeeni" />
-        <div className="search">
-          <i className='fab fa-sistrix icon-search'></i>
-          <input type="text" placeholder='Recherche...' />
+      <div className="search">
+        <i className='fab fa-sistrix icon-search'></i>
+        <input type="text" placeholder='Recherche...' />
+      </div>
+      <div className="user-info">
+        <div className="notif" />
+        <img src={avatar} alt="image avatar" />
+        <div className="detail-user">
+          <div className="detail">Team Const</div>
+          <div className="level">Maitre</div>
         </div>
-        <div className="user-info">
-          <div className="notif" />
-          <img src={avatar} alt="image avatar" />
-          <div className="name">Team Const</div>
-        </div>
+      </div>
     </div>
   )
 }
