91092bae3806da88b1bba4c5e496ff6f80277eb7
lando00 <orlandojulien2@gmail.com>
2023-03-04 10:18:24 +0100

    projet depart

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
 import React from 'react'
-import DashboardContainer from './components/DashboardContainer/DashboardContainer'
-import Authentification from './components/Authentification/Authentification'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Tables from './components/Tables/Tables'
-import Products from './components/Products/Products'
-import Dashboard from './components/Dashboard/Dashboard'
-import Settings from './components/Settings/Settings'
-import ListUsers from './components/Users/ListUsers/ListUsers'
-import AddUser from './components/Users/AddUser/AddUser'
-import UpdateUser from './components/Users/UpdateUser/UpdateUser'
+import Container from './components/EspaceEtudiant/Container/Container'
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Authentification />} />
-        <Route path="/app" element={<DashboardContainer />}>
-          <Route path='/app/dashboard' element={<Dashboard />} />
-          <Route path='/app/users/listUsers' element={<ListUsers />} />
-          <Route path='/app/users/addUser' element={<AddUser />} />
-          <Route path='/app/users/updateUser/:id' element={<UpdateUser />} />
-          <Route path='/app/tables' element={<Tables />} />
-          <Route path='/app/products' element={<Products />} />
-          <Route path='/app/settings' element={<Settings />} />
-        </Route>
+        <Route path="/" element={<Container />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/EspaceEtudiant/Container/Container.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Container.scss'
+
+export default function Container() {
+  return (
+    <div>
+      <h1>Container</h1>
+    </div>
+  )
+}
