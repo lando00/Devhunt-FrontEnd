871ad85d331df7303d8f30cf7e601f0667057f44
N-mika <nadrasanamichael.com>
2023-03-05 18:15:28 +0300

    dernier commit sur git

--- src/components/EspaceEtudiant/Actuality/Actuality.jsx
+++ src/components/EspaceEtudiant/Actuality/Actuality.jsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react'
 import "./Actuality.scss"
 import { NavLink, Outlet } from 'react-router-dom'
 export default function Actuality() {
+  const getOutet = <Outlet />;
   const change = () => {
-    const getOutet = <Outlet />;
-    const vu = getOutet.key ? 'ok' : 'ko'
+    const vu = getOutet ? 'ok' : 'ko'
     console.log(vu, getOutet)
   }
+  const vu = getOutet ? 'ok' : 'ko'
+  console.log(getOutet , vu);
   return (
     <div className='actuality'>
       <div className="head-actuality">
