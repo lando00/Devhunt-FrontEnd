5dbafbcafb87c9c9a86e0048adb8d062f344649d
Hendrilalaina <hendrilalaina@gmail.com>
2023-03-10 09:33:52 +0300

    Login final update

--- src/components/EspaceAdmin/Authentification/Login/Login.jsx
+++ src/components/EspaceAdmin/Authentification/Login/Login.jsx
@@ -1,42 +1,70 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Login.css';
 import axios from 'axios';
 
 export default function Login() {
 
-  const [serialNumber, setSerialNumber] = useState('');
-  const [passWord, setPassWord] = useState('');
+  const [serialNumber, setSerialNumber] = useState();
+  const [passWord, setPassWord] = useState();
+  const [user, setUser] = useState({
+    student: {},
+    authToken: ''
+  })
 
-  const login = () => {
-    axios.get("/devHunt2/students")
-    .then(rep => {
-      console.log(rep)
+  const navigate = useNavigate();
+
+  // const login = async () => {
+  //   axios
+  //     .post("/devHunt2/login", {
+  //       serialNumber,
+  //       passWord
+  //     })
+  //     .then(response => {
+  //       const authenticated = response.data.data
+  //       authentication(authenticated);
+  //       // navigate('/actuality')
+  //     })
+  //     .catch(err => {
+  //       console.log(err)
+  //     })
+  // }
+
+  const login = async (event) =>{
+    event.preventDefault();
+    const response = await axios.post('/devHunt2/login', {
+      serialNumber,
+      passWord
     })
-    .catch(err => {
-      console.log(err)
-    })
+    setUser(user =>({
+      ...user,
+      ...response.data.data
+    }))
   }
 
+  useEffect(() => {
+    console.log(user)
+  }, [user])
+
   return (
-    <div className='loginContainer'>
-      <div className="login">
-        <h1>Welcome ! 👋</h1>
-        <label className='label' htmlFor='serialNumber'>Num&eacute;ro matricule</label>
-        <input type="text" placeholder='example@gmail.com' id='serialNumber' onChange={(e) => setSerialNumber(e.target.value)}/>
-        <label className='label' htmlFor='passWord'>Mot de passe</label>
-        <input type="password" placeholder='**********' id='passWord' onChange={(e) => setPassWord(e.target.value)} />
-        <p className='remember'>
-          <input type="checkbox" id='remember' />
-          <label htmlFor="remember">Se souvient de moi</label>
-          <span>Mot de passe oublié?</span>
-        </p>
+      <div className='loginContainer'>
+        <div className="login">
+          <h1>Welcome ! 👋</h1>
+          <label className='label' htmlFor='serialNumber'>Num&eacute;ro matricule</label>
+          <input type="text" placeholder="Numéro d'inscription" id='serialNumber' onChange={(e) => setSerialNumber(e.target.value)}/>
+          <label className='label' htmlFor='passWord'>Mot de passe</label>
+          <input type="password" placeholder='**********' id='passWord' onChange={(e) => setPassWord(e.target.value)} />
+          <p className='remember'>
+            <input type="checkbox" id='remember' />
+            <label htmlFor="remember">Se souvient de moi</label>
+            <span>Mot de passe oublié?</span>
+          </p>
 
-        <Link to="#" onClick={login}>
-            Login
-        </Link>
+          <button onClick={login}>
+              Login
+          </button>
 
+        </div>
       </div>
-    </div>
   )
 }
