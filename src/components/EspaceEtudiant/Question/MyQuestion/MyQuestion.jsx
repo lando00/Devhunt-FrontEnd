e27dd24c8b1de35a0730c56233e8eed2de118835
Hendrilalaina <hendrilalaina@gmail.com>
2023-03-10 23:22:49 +0300

    Fetch question

--- src/components/EspaceEtudiant/Question/MyQuestion/MyQuestion.jsx
+++ src/components/EspaceEtudiant/Question/MyQuestion/MyQuestion.jsx
@@ -1,12 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import avatar from '../../../../assets/avatars/face-3.jpg'
 import { NavLink } from 'react-router-dom'
 import { questions } from '../../../../data/AllData';
 import Question from '../QuestionItems/QuestionItem';
+import axios from 'axios';
 export default function MyQuestion() {
+
+  const [questions, setQuestions] = useState([])
+
+  const idStudent = JSON.parse(localStorage.getItem('user'));
+  useEffect(() =>{
+    const getQuestions = async(id) => {
+      const response  = await axios.get(`/devHunt2/students/posts/${id}`)
+      setQuestions(response.data.data)
+    }
+    getQuestions(idStudent);
+  },[])
+
+  const myQuestionsItems = questions.map((question) => {
+    return (
+      <Question question={question}></Question>
+    )
+  })
+
   return (
     <div className='my-question'>
-      <Question questions={questions}></Question>
+      {myQuestionsItems}
     </div>
   )
 }
--- src/components/EspaceEtudiant/Question/QuestionItems/QuestionItem.jsx
+++ src/components/EspaceEtudiant/Question/QuestionItems/QuestionItem.jsx
@@ -1,29 +1,34 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import avatar from '../../../../assets/avatars/face-8.jpg'
-export default function QuestionItem({questions}) {
-  const post = questions.map(({ nbrResponse, isResolved, name, tittle, post, lasteName, date , id}) => (
-    <div key={id} className="actuality-item">
-      <img src={avatar} alt="" className='pdp' />
-      <div className="detail-post">
-        <div className="head-post">
-          <div className="name">{lasteName} {name}</div>
-          <div className="date"> {date} </div>
-        </div>
-        <div className="detail">
-          <div className="tittle"> {tittle} {isResolved ? <div className="resolved">(Résolue)</div> : <div className="unresolved" >(Non résolue)</div>}</div>
-          <div className="post"> {post}</div>
-          <div className="replay">
-            <NavLink to={`/questions/question/${id}`}>
-              <div className="answer">Reponse : {nbrResponse}</div>
-            </NavLink>
-          </div>
-        </div>
-      </div>
-    </div>))
+export default function QuestionItem({question}) {
   return (
     <div className='QuestionItem'>
-      {post }
+
+      <div key={question.id} className="actuality-item">
+
+        <img src={avatar} alt="" className='pdp' />
+        
+        <div className="detail-post">
+          <div className="head-post">
+            <div className="name">{lasteName} {name}</div>
+            <div className="date"> {date} </div>
+          </div>
+          
+          <div className="detail">
+            <div className="tittle"> {tittle} {isResolved ? <div className="resolved">(Résolue)</div> : <div className="unresolved" >(Non résolue)</div>}</div>
+            <div className="post"> {post}</div>
+            <div className="replay">
+              <NavLink to={`/questions/question/${id}`}>
+                <div className="answer">Reponse : {nbrResponse}</div>
+              </NavLink>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
   )
 }
