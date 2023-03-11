e97b5c3314bf75c6868e2cf8298759e0f372ea28
Ludores <Ludores22@gmail.com>
2023-03-11 05:42:50 +0100

    stream video

--- src/components/EspaceEtudiant/Live/LiveEnCours/LiveEnCours.jsx
+++ src/components/EspaceEtudiant/Live/LiveEnCours/LiveEnCours.jsx
@@ -7,9 +7,20 @@ export default function LiveEnCours() {
   const aa = [1, 2, 3, 2, 1, 3, 1];
   const [showProgramme, setshow] = useState(false)
   const [live, showLive] = useState(false)
+  const [isOwner, becomeOwner] = useState(false)
+
+  function create_my_live(){
+    becomeOwner(true)
+    showLive(true)
+  }
+
+  function see_live_other(){
+    becomeOwner(false)
+    showLive(true)
+  }
   return (
     <div className='LiveEnCours'>
-      <div className="new-live" onClick={() => showLive(true)}>
+      <div className="new-live" onClick={create_my_live}>
         show live
       </div>
       {!showProgramme && (<div className="new-live" onClick={() => setshow(!showProgramme)}>
@@ -17,7 +28,7 @@ export default function LiveEnCours() {
       </div>)}
       {!showProgramme && <div className="list">
         {aa.map(element => {
-          return <LiveItem />
+          return <div onClick={see_live_other}><LiveItem/> </div>
         })}
       </div>}
       {
@@ -25,7 +36,7 @@ export default function LiveEnCours() {
           Direct en cours
         </div>
       }
-      {live && <LiveToLive showSetting={showLive} />}
+      {live && <LiveToLive showSetting={showLive} isOwner= {isOwner}/>}
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/devHunt2": "http://192.168.137.106:8000",
+      "/devHunt2": "http://localhost:8000",
     },
   },
   plugins: [react()],
